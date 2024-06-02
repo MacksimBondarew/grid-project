519d055278967814351dbf8a29fa78f011b52b82
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2024-06-02 14:10:05 +0200

    part 2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
-$(function(){
+"use strict";
 
+const tabItem = document.querySelectorAll(".tabs__btn-item");
+const tabContent = document.querySelectorAll(".tabs__content-item");
+
+tabItem.forEach((element) => {
+    element.addEventListener("click", open);
 });
+
+function open(event) {
+    const tabTarget = event.currentTarget;
+    console.log(tabTarget)
+    const button = tabTarget.dataset.button;
+    console.log(button)
+
+    tabItem.forEach((element) => {
+        element.classList.remove("tabs__btn-item--active");
+    });
+    tabContent.forEach((element) => {
+        element.classList.remove("tabs__content-item--active");
+    });
+    tabTarget.classList.add("tabs__btn-item--active");
+    document.querySelector(`#${button}`).classList.add("tabs__content-item--active");
+}
